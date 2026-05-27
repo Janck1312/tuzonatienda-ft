cfb61889f6441fb2a6e397208c641add49fd51fb
Janck <sepulved.janck@gmail.com>
2026-05-27 19:07:25 -0400

    [FEAT]: add header navigate to react router

--- app/components/common/header.tsx
+++ app/components/common/header.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { Link } from 'react-router';
+import { useNavigate } from 'react-router';
 import { AppContext } from '~/context/app.context';
 
 export default function Header() {
@@ -6,37 +8,40 @@ export default function Header() {
         user: any;
         setUser: React.Dispatch<React.SetStateAction<any>>;
     }>(AppContext);
-
+    const navigate = useNavigate();
+    
     return (
         <header className="flex px-4 py-6 bg-primary">
             <h1>Tu zona tienda</h1>
             <nav className="flex ml-auto mr-0 py-2">
                 <ul className="flex gap-4 mr-0 ml-auto">
-                    <li>
-                        <a href="/">Principal</a>
+                    <li onClick={() => navigate('/')}>
+                        <Link to="/">Principal</Link>
+                    </li>
+                    <li onClick={() => navigate('/about')}>
+                        <Link to="/about">Sobre nosotros</Link>
+                    </li>
+                    {user?.id && (
+                        <li onClick={() => navigate('/products')}>
+                            <Link to="/products">Productos</Link>
+                        </li>
+                    )}
+                    <li onClick={() => navigate('/contact')}>
+                        <Link to="/contact">Contacto</Link>
+                    </li>
+                    <li onClick={() => navigate('/signup')}>
+                        <Link to="/signup">Registrate</Link>
+                    </li>
+                    <li onClick={() => navigate('/login')}>
+                        <Link to="/login">Ingresa</Link>
                     </li>
                     <li>
-                        <a href="/about">Sobre nosotros</a>
-                    </li>
-                    <li>
-                        <a href="">Productos</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contacto</a>
-                    </li>
-                    <li>
-                        <a href="/register">Registrate</a>
-                    </li>
-                    <li>
-                        <a href="">Ingresa</a>
-                    </li>
-                    <li>
-                        <a href="#" title="Mis compras">
+                        <Link to="#" title="Mis compras">
                             <i
                                 className="pi pi-shopping-cart"
                                 title="Mis compras"
                             ></i>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </nav>
